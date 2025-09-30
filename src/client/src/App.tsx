af9e2ae47b4636bb96f7d4e18b0acada9d7dfa42
SpenyT <spencer.toupin@outlook.com>
2025-09-30 10:06:48 -0400

    #US.18: #Task.62 - added default React Router with paths and children, and implemented it in App.tsx

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,9 +1,12 @@
-import './App.css'
+import { RouterProvider } from 'react-router/dom';
+import router from './routing/reactRouter.ts';
 
 export default function App() {
 
   return (
+    // future clerk provider implementation
     <>
+      <RouterProvider router={router}/>
     </>
   )
 }
---
+++ src/client/src/routing/reactRouter.ts
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from "react-router";
+
+import RootLayout from "./RootLayout";
+import Home from "../pages/Home";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        Component: RootLayout,
+        children: [
+            { index: true, Component: null }, // set to null for now, but should be replaced by the Home page component
+            {
+                path: "dashboard",
+                Component: null, // set to null for now, but should be replaced by the Dashboard page component
+                children: [] // where the different Dashboard tabs will be
+            },
+            { path: "/search", Component: null} // set to null for now, but should be replaced by the Search page component
+        ],
+    },
+]);
+
+export default router;
