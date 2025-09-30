3c932ddcfae99e3abf91bcde8e1eefc4a47cdd5e
SpenyT <spencer.toupin@outlook.com>
2025-09-30 10:02:58 -0400

    #US.18: #Task.61 - Root Layout default implementation

---
+++ src/client/src/routing/RootLayout.tsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router";
+
+export default function RootLayout() {
+    return ( 
+        // future navbar implementation likely
+        <> 
+            <Outlet />
+        </>
+        // future footer implementation likely
+    )
+}
