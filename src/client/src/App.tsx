fb709ed9c120bc6be0371b9aadd3f037b72dee43
ϻartin <51332188+martin0024@users.noreply.github.com>
2025-10-01 19:48:14 -0400

    feat(Theme): implement toggle component for light/dark mode

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,11 +1,14 @@
 import { RouterProvider } from "react-router/dom";
 import router from "./routing/reactRouter.tsx";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import { ThemeToggle } from "./components/ThemeToggle";
 
 export default function App() {
    return (
-      // future clerk provider implementation
-      <>
+      <ThemeProvider>
+         {/* future clerk provider implementation */}
          <RouterProvider router={router} />
-      </>
+         <ThemeToggle />
+      </ThemeProvider>
    );
 }
