3cbb1abe6a9375c66e39e79bae04b14e4e245a12
ϻartin <51332188+martin0024@users.noreply.github.com>
2025-10-01 13:21:42 -0400

    feat(ErrorBoundary): add ErrorBoundary component for handling route errors

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,12 +1,11 @@
-import { RouterProvider } from 'react-router/dom';
-import router from './routing/reactRouter.ts';
+import { RouterProvider } from "react-router/dom";
+import router from "./routing/reactRouter.tsx";
 
 export default function App() {
-
-  return (
-    // future clerk provider implementation
-    <>
-      <RouterProvider router={router}/>
-    </>
-  )
+   return (
+      // future clerk provider implementation
+      <>
+         <RouterProvider router={router} />
+      </>
+   );
 }
